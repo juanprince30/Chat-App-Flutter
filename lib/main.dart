5fcb0ecbb2f00a471e87c9f7d1a6b7c4a1f0ea97
Bassana07 <moussa60783979@gmail.com>
2024-12-06 15:17:52 +0000

    intro pge

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+
       initialRoute: "/intro",
       routes: {
         "/intro": (context) => Intro(),
