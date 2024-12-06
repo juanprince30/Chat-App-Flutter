d64f736030be70c2825ee589c2ce0e622cfd19d6
Bassana07 <moussa60783979@gmail.com>
2024-12-06 16:52:07 +0000

    intro pge

--- lib/vues/intro.dart
+++ lib/vues/intro.dart
@@ -77,17 +77,37 @@ class Intro extends StatelessWidget {
                         child: TextFormField(
                           keyboardType: TextInputType.phone,
                           decoration: InputDecoration(
+                            hintText: "Entrez votre numéro",
                             filled: true,
                             fillColor: Colors.white,
                             border: OutlineInputBorder(
                               borderRadius: BorderRadius.circular(10.0),
                             ),
-                            contentPadding: const EdgeInsets.symmetric(
-                                horizontal: 16.0),
+                            contentPadding:
+                            const EdgeInsets.symmetric(horizontal: 16.0),
                           ),
                         ),
                       ),
                     ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(40, 20, 40, 0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Action à effectuer
+                      print("Numéro de téléphone soumis");
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blueAccent,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: const Text(
+                      "Continuer",
+                      style: TextStyle(color: Colors.white, fontSize: 16),
+                    ),
                   ),
                 ),
               ],
